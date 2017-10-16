ef6939a46fa0f5eb6d2a8de6a334adf659c78be2
Alison Ostendorf <aostendorf@gmail.com>
2017-10-16 10:50:08 -0400

    create routes for initial pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+	def about
+	end
+
+	def community
+	end
+
+	def design
+	end
+
+	def faq
+	end
+
+	def supplies
+	end
+
+	def thinking
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "pages#supplies"
+  get "about" => "pages#about"
+  get "design" => "pages#design"
+  get "faq" => "pages#faq"
+  get "supplies" => "pages#supplies"
+  get "thinking" => "pages#thinking"
+
 end

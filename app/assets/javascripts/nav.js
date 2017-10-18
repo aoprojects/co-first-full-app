05ed65e1d1055e4fdd96bd5a17c4ef50d803b230
Alison Ostendorf <aostendorf@gmail.com>
2017-10-17 20:27:14 -0400

    add click functionality for nav. Not done yet.

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,1 +1,7 @@
-nav.js
+$( document ).ready(function() {
+	$( "#navigation-hamburger" ).click(function() {
+		event.stopImmediatePropagation();
+		$( "#navigation-list" ).toggle( "fast" );
+		$(this).toggleClass("hamburger_clicked");
+	});
+});

5bfad24e74158fdee78767ff036e570546f9c76b
Alison Ostendorf <aostendorf@gmail.com>
2017-11-15 13:53:51 -0500

    remove extra text in hamburger area manu icon

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,12 +1,13 @@
 $( document ).ready(function() {
 	var $navigation_hamburger = $("#navigation-hamburger");
 	var $navigation_list = $("#navigation-list");
-	$(document).on('click', "$navigation_hamburger", function() {
+	$(document).on("click", "#navigation-hamburger", function() {
 		console.log('clicked');
-		$('#navigation-hamburger').toggle("fast");
+		$("#navigation-hamburger").toggle("fast");
 			$navigation_list.toggleClass("toggled");
 			$navigation_hamburger.toggleClass("hamburger_clicked");
-			$("main").toggleClass("lowered")''
+			$("main").toggleClass("lowered");
+			$("footer").toggleClass("lowered");
 // add something to move the footer too
 
 	});

cdb2bf83dbd0cbb62e29eb6c6577538ea429ca50
Alison Ostendorf <aostendorf@gmail.com>
2017-11-15 13:59:52 -0500

    clean up js file a bit. Still not dropping nav

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,21 +2,11 @@ $( document ).ready(function() {
 	var $navigation_hamburger = $("#navigation-hamburger");
 	var $navigation_list = $("#navigation-list");
 	$(document).on("click", "#navigation-hamburger", function() {
-		console.log('clicked');
-		$("#navigation-hamburger").toggle("fast");
+		console.log("clicked");
+		$navigation_hamburger.toggle("fast");
 			$navigation_list.toggleClass("toggled");
 			$navigation_hamburger.toggleClass("hamburger_clicked");
 			$("main").toggleClass("lowered");
 			$("footer").toggleClass("lowered");
-// add something to move the footer too
-
 	});
-
-	// $( "#navigation-hamburger" ).click(function() {
-	// 	event.stopImmediatePropagation();
-	// 	$( "#navigation-list" ).toggleClass( "toggled" );
-	// 	$(this).toggleClass("hamburger_clicked");
-	// 	$("main").toggleClass("lowered");
-	// });
 });
-// todo be able to click hamburger after clicked nav links already

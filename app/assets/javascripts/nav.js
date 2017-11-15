c70ce8ccccfcb94ab70f51671e219509cb1cde58
Alison Ostendorf <aostendorf@gmail.com>
2017-11-15 14:10:23 -0500

    get nav to drop down at least once

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,12 +1,13 @@
 $( document ).ready(function() {
 	var $navigation_hamburger = $("#navigation-hamburger");
 	var $navigation_list = $("#navigation-list");
-	$(document).on("click", "#navigation-hamburger", function() {
+	$(document).on("click", $navigation_hamburger, function() {
 		console.log("clicked");
-		$navigation_hamburger.toggle("fast");
-			$navigation_list.toggleClass("toggled");
-			$navigation_hamburger.toggleClass("hamburger_clicked");
-			$("main").toggleClass("lowered");
-			$("footer").toggleClass("lowered");
+		$navigation_list.toggle("fast");
+		stopImmediatePropagation();
+		// $navigation_list.toggleClass("toggled");
+		// $navigation_hamburger.toggleClass("hamburger_clicked");
+		// $("main").toggleClass("lowered");
+		// $("footer").toggleClass("lowered");
 	});
 });

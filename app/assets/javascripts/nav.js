8afdee0393c6096fe536fff59be299dacd07cd75
Alison Ostendorf <aostendorf@gmail.com>
2017-11-14 20:46:37 -0500

    alter js file so that it has a similar structure to the RR application

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
-	$( "#navigation-hamburger" ).click(function() {
-		event.stopImmediatePropagation();
-		$( "#navigation-list" ).toggleClass( "toggled" );
-		$(this).toggleClass("hamburger_clicked");
-		$("main").toggleClass("lowered");
+	var $navigation_hamburger = $("#navigation-hamburger");
+	var $navigation_list = $("#navigation-list");
+	$(document).on('click', "$navigation_hamburger", function() {
+		console.log('clicked');
+		$('#navigation-hamburger').toggle("fast");
+			$navigation_list.toggleClass("toggled");
+			$navigation_hamburger.toggleClass("hamburger_clicked");
+			$("main").toggleClass("lowered")''
+// add something to move the footer too
+
 	});
+
+	// $( "#navigation-hamburger" ).click(function() {
+	// 	event.stopImmediatePropagation();
+	// 	$( "#navigation-list" ).toggleClass( "toggled" );
+	// 	$(this).toggleClass("hamburger_clicked");
+	// 	$("main").toggleClass("lowered");
+	// });
 });
 // todo be able to click hamburger after clicked nav links already

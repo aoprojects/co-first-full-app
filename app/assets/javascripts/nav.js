2e17df9a8701e222d46def7652355a7cebe22498
Alison Ostendorf <aostendorf@gmail.com>
2017-12-11 18:36:56 -0800

    specify the object that gets clicked on jquery hamburger drop

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
 	var $navigation_hamburger = $("#navigation-hamburger");
 	var $navigation_list = $("#navigation-list");
-	$(document).on("click", $navigation_hamburger, function() {
+	$navigation_hamburger.on("click", function() {
 		console.log("clicked");
 		$navigation_list.toggle("fast");
-		// $navigation_list.toggleClass("visible");
-		$("main").toggleClass("lowered");
-		$("footer").toggleClass("lowered");
-		$navigation_list.toggleClass("toggled");
-		$navigation_hamburger.toggleClass("hamburger_clicked");
-		stopImmediatePropagation();
+			// $navigation_list.toggleClass("visible");
+			$("main").toggleClass("lowered");
+			$("footer").toggleClass("lowered");
+			// $navigation_list.toggleClass("toggled");
+			$navigation_hamburger.toggleClass("hamburger_clicked");
+			stopImmediatePropagation();
 	});
 });

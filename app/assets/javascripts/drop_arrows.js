3aa0548dcecc19dba694789489540158fb3e8f19
Alison Ostendorf <aostendorf@gmail.com>
2017-12-11 19:15:46 -0800

    confirm first structure of faq styling

---
+++ app/assets/javascripts/drop_arrows.js
@@ -0,0 +1,9 @@
+// $( document ).ready(function() {
+// 	$( ".rate_category").click(function() {
+// 		$(this).next(".rate_category_details").toggle("fast");
+// 		$(this).find("span.before-toggle").toggle();
+// 		$(this).find("span.after-toggle").toggle();
+// 	});
+// });	
+
+// this code is taken from an old site and these classes don't pertain to this site. Fix. 
